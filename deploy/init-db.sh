c74df077bb1e8aa0dda014797a221465dc1e0164
ltzaqmlp <1923443698@qq.com>
2026-09-08 14:17:51 +0800

    10

--- deploy/init-db.sh
+++ deploy/init-db.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -eu
 psql -v ON_ERROR_STOP=1 --username postgres --dbname shop_operations -v app_password="$APP_PASSWORD" <<'SQL'
-CREATE ROLE shop_app LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD :'app_password';
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'shop_app') THEN
+    CREATE ROLE shop_app LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE;
+  END IF;
+END
+$$;
+ALTER ROLE shop_app LOGIN PASSWORD :'app_password';
 ALTER DATABASE shop_operations OWNER TO shop_app;
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 GRANT USAGE, CREATE ON SCHEMA public TO shop_app;
